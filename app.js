324ee0b164453f3fdb5a04b843e22ffe36f73bcd
PreyZer <81478765+PreyanshThaker@users.noreply.github.com>
2021-06-02 11:40:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+// Requiring the packages and modules
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-
+// Creating the app
 const app = express();
+// Getting everything ready
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+// In case of any errors
 app.get("/error", function(req, res) {
   res.send("<h1>That's an error.</h1>");
 });
-
+// Render the Home Page
 app.get("/", function(req, res){
   res.render("email");
 });
-
+// Handling the post request from the home route
 app.post("/", function(req, res) {
     var transporter = nodemailer.createTransport({
         service: req.body.provider,
@@ -31,7 +33,7 @@ app.post("/", function(req, res) {
         subject: req.body.subject,
         text: req.body.message
       };
-      
+      //Sending the mail
       transporter.sendMail(mailOptions, function(error, info){
         if (error) {
           console.log(error);
@@ -41,7 +43,7 @@ app.post("/", function(req, res) {
         }
       }); 
 });
-
+//Listening on Port 3000
 app.listen(3000, function() {
   console.log("Server is listening on port 3000.")
 });
